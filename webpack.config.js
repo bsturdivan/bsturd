1eb9591acfc46e3391b706d1e41414b71e314169
Brian Sturdivan <bsturdivan@gmail.com>
2023-07-11 00:02:18 -0700

    update sitemap

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 const RobotstxtPlugin = require('robotstxt-webpack-plugin')
-const SitemapPlugin = require('sitemap-webpack-plugin').default
 
 module.exports = {
   mode: 'development',
@@ -37,20 +36,6 @@ module.exports = {
       },
     }),
     new FaviconsWebpackPlugin('./src/images/favicon.svg'),
-    new SitemapPlugin({
-      base: 'https://bsturd.com',
-      paths: [
-        {
-          path: '/',
-          lastmod: true,
-        },
-      ],
-      options: {
-        filename: 'sitemap.xml',
-        lastmod: true,
-        priority: 1.0,
-      }
-    }),
     new RobotstxtPlugin({ 
       policy: [
       {
